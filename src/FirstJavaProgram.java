00ef0917e87e52e3c445610d30b3bf2bd9b68911
nixswinner <nixswinner@gmail.com>
2020-05-20 12:20:50 +0300

    initial commit

---
+++ src/FirstJavaProgram.java
@@ -0,0 +1,16 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello my first program");
+    }
+
+}
+/*<?php
+echo(
+?>
+* function add(){
+* return
+* }
+* echo
+* printf
+* print
+* */
---
+++ src/JavaVariables.java
@@ -0,0 +1,58 @@
+import java.awt.*;
+
+public class JavaVariables {
+    public static void main(String[] args) {
+        //single line comment
+        /*
+       ----block comments
+         */
+        /*
+        * Java is considered a statically typed language
+        * */
+        //data -type -[strings "",
+        // integer int -whole numbers ,float - decimal places
+        // double -
+        // byte
+        // char "A"]
+        //store name and age of person
+        //camel case  myschoolname = mySchoolName
+        String personName = "John";//declared a variable
+        int age = 24;
+        System.out.println(personName);
+        System.out.println(age);
+
+        //Data types- dictate which operation applied to those
+        //Types.
+        /*
+        * 1.Primitives - 8 types
+        * boolean  - true or false
+        * char - "M" "F" - one character
+        * byte - 4bits
+        * short
+        * int
+        * long
+        * float
+        * double
+        * */
+        char genderAbr = 'F';
+        boolean status = true;
+        /*
+        * +, - ./ *
+        * */
+        int x =10;
+        int y =20;
+        int sum = x+y;
+
+
+
+
+    }
+
+}
+/*
+How we declare variables in java
+
+[data-type variable_name = value]
+
+* php $name = 10;
+* */
